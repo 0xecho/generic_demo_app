3c91ccd2194e459487f6268d3d6baf00540be890
0xecho <0xecho@proto-code.com>
2022-03-01 23:03:11 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f --set-upstream git@github.com:0xecho/generic_demo_app.git master
+
+cd -
